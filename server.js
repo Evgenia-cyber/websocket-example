ae9502e654d1e9a6b5273e1de891a77fb8cad981
Evgenia Kurineva <kurineva85@mail.ru>
2021-09-02 16:15:10 +0300

    feat: send message from server to client and display this message in browser

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   console.log('A user connected');
 
+  //Send a message to the client after a timeout of 4seconds
+  setTimeout(function () {
+   console.log('A user send message to the client');
+    socket.send('Sent a message 4seconds after connection!');
+  }, 4000);
+
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
     console.log('A user disconnected');
