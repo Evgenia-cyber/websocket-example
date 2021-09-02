65071e8051382645c6055a6b935b11b0bdbc9a61
Evgenia Kurineva <kurineva85@mail.ru>
2021-09-02 16:05:12 +0300

    feat: add socket.io on server and on client

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
 });
 
 http.listen(3000, function () {
